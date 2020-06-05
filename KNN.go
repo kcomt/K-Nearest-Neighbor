2e45d73298d90abcff422abbf8f4307edfc41d6f
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2020-06-05 13:37:44 -0500

    .

--- KNN.go
+++ KNN.go
@@ -6,39 +6,42 @@ import (
 )
 
 type data struct {
+	id     int
 	width  float64
 	size   float64
 	length float64
 	value  string
 }
 
-func distance(x float64, y float64, x2 float64, y2 float64) float64 {
-	result1 := math.Pow((x - x2), 2)
-	result2 := math.Pow((y - y2), 2)
-	result := math.Sqrt(result1 + result2)
+func distance(predict data, train data) float64 {
+	result1 := math.Pow((predict.width - train.width), 2)
+	result2 := math.Pow((predict.size - train.size), 2)
+	result3 := math.Pow((predict.length - train.length), 2)
+	result := math.Sqrt(result1 + result2 + result3)
 	return result
 }
 
-func predict(predict data, array []data) data {
-	for v := range array {
-		fmt.Println(array[v].length)
+func predict(predict data, array []data) float64 {
+	dist := 0.5
+	for i := 0; i < len(array); i++ {
+		dist = distance(predict, array[i])
 	}
-	return predict
+	return dist
 }
 
 func main() {
 	array := make([]data, 0, 20)
-	data1 := data{100, 100, 100, "red"}
-	data2 := data{89, 88, 91, "red"}
-	data3 := data{102, 103, 104, "red"}
-	data4 := data{90, 91, 92, "red"}
-	data5 := data{79, 60, 80, "red"}
+	data1 := data{0, 100, 100, 100, "red"}
+	data2 := data{1, 89, 88, 91, "red"}
+	data3 := data{2, 102, 103, 104, "red"}
+	data4 := data{3, 90, 91, 92, "red"}
+	data5 := data{4, 79, 60, 80, "red"}
 
-	data6 := data{0, 0, 0, "blue"}
-	data7 := data{10, 16, 11, "blue"}
-	data8 := data{6, 13, 32, "blue"}
+	data6 := data{5, 0, 0, 0, "blue"}
+	data7 := data{6, 10, 16, 11, "blue"}
+	data8 := data{8, 6, 13, 32, "blue"}
 
-	predict := data{80, 70, 104, "None"}
+	predict := data{3, 80, 70, 104, "None"}
 
 	array = append(array, data1, data2, data3, data4, data5, data6, data7, data8)
 
